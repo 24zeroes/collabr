1d1317d59b20ca29b040ff869d0f63a9f4a1b8b7
24zeroes <vdmbat@gmail.com>
2021-08-02 13:11:58 +0300

    Event message expanded

--- src/client/index.js
+++ src/client/index.js
@@ -4,17 +4,26 @@
 
     ws.onmessage = (webSocketMessage) => {
         const messageBody = JSON.parse(webSocketMessage.data);
-        let text = 'color= ' + messageBody.color + 'key= ' + messageBody.key + ' code= ' + messageBody.code +    (e.shiftKey ? ' shiftKey' : '') +
-        (e.ctrlKey ? ' ctrlKey' : '') +
-        (e.altKey ? ' altKey' : '') +
-        (e.metaKey ? ' metaKey' : '') +
-        (e.repeat ? ' (repeat)' : '');
+        let text = 'color= ' + messageBody.color + 'key= ' + messageBody.key + ' code= ' + messageBody.code +    
+        (messageBody.shiftKey ? ' shiftKey' : '') +
+        (messageBody.ctrlKey ? ' ctrlKey' : '') +
+        (messageBody.altKey ? ' altKey' : '') +
+        (messageBody.metaKey ? ' metaKey' : '') +
+        (messageBody.repeat ? ' (repeat)' : '');
     
         console.log(text);
     };        
     
     document.body.onkeypress = (evt) => {
-        const messageBody = { code: evt.code, key: evt.key };
+        const messageBody = { 
+            code: evt.code, 
+            key: evt.key, 
+            shiftKey: evt.shiftKey,
+            ctrlKey: evt.ctrlKey,
+            altKey: evt.altKey,
+            metaKey: evt.metaKey,
+            repeat: evt.repeat,
+        };
         ws.send(JSON.stringify(messageBody));
     };
         
