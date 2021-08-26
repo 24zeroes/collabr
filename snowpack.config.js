7b41f31f03c43f225975294520efa963e06dbd0c
24zeroes <vdmbat@gmail.com>
2021-08-27 01:30:40 +0300

    Snowpack config actualized

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,53 +1,13 @@
 const proxy = require('http2-proxy')
 
-/** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
     "client/public": "/",
-    "client/src": "/dist",
+    "client/src": "/dist/",
   },
   routes: [
     {
-      src: '/ws',
-      upgrade: (req, socket, head) => {
-
-        const defaultWSHandler = (err, req, socket, head) => {
-          if (err) {
-            console.error('proxy error', err);
-            socket.destroy();
-          }
-        };
-
-        const defaultWebHandler = (err, req, res) => {
-          if (err) {
-            console.error('proxy error', err)
-            finalhandler(req, res)(err)
-          }
-        }
-
-        proxy.ws(
-          req,
-          socket,
-          head,
-          {
-            hostname: 'localhost',
-            port: 7071,
-          },
-          defaultWSHandler,
-        );
-
-        proxy.web(
-          req, 
-          res, 
-          {
-            hostname: 'localhost',
-            port: 3000, 
-          },
-          defaultWebHandler
-        );
-      },
-    },
-    {
+      match: 'all',
       src: '/api/.*',
       dest: (req, res) => {
         // remove /api prefix (optional)
@@ -57,6 +17,11 @@ module.exports = {
           port: 3000,
         });
       },
+    },
+    {
+      match: 'routes',
+      src: '.*',
+      dest: '/index.html',
     },
   ],
 };
