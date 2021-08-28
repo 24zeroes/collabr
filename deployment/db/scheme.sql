3fa6854aeaea6d2a9e926eb4a3627873a2a93fb5
24zeroes <vdmbat@gmail.com>
2021-08-29 01:31:08 +0300

    Api routes moved to main index file; WS endpoint removed;

--- deployment/db/scheme.sql
+++ deployment/db/scheme.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS DocumentContent(
   id SERIAL PRIMARY KEY,
-  content JSON,
+  content JSON
 );
 
 CREATE TABLE IF NOT EXISTS Documents(
   id SERIAL PRIMARY KEY,
   name VARCHAR,
   maskedName VARCHAR,
-  contentId VARCHAR REFERENCES DocumentContent(id),
+  contentId SERIAL REFERENCES DocumentContent(id)
 );
--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,20 @@
-// Shared objects
-const sessions = new Map();
+// RestApi setup
+const express = require('express')
+const app = express()
+const config = require('config');
+const restPort = config.get('api.port');
 
-const wsServer = require('./wss/init');
-wsServer.init(sessions);
+const { getDocuments, createDocument } = require('./handlers/document/index');
 
-const restServer = require('./rest/init');
-restServer.init(sessions);
+app.use(express.json());
+app.post('/document/create', ((request, response, next) => asyncUtil(createDocument(request, response, next))));
+
+app.get('/documents', ((request, response, next) => asyncUtil(getDocuments(request, response, next))));
+app.listen(restPort);
+
+const asyncUtil = fn =>
+function asyncUtilWrap(...args) {
+  const fnReturn = fn(...args)
+  const next = args[args.length-1]
+  return Promise.resolve(fnReturn).catch(next)
+}
